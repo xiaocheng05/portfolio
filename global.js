74da185aaad9cab35b56dee7fb8d688c7d57586d
xiaocheng05 <xiaocheng0528@icloud.com>
2026-04-17 21:49:46 -0700

    fix: host on github

--- global.js
+++ global.js
@@ -18,7 +18,7 @@ function $$(selector, context = document) {
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                  // Local server
-    : "/website/";         // GitHub Pages repo name
+    : "/portfolio/";         // GitHub Pages repo name
 
 let pages = [
     { url: '', title: 'Home' },
